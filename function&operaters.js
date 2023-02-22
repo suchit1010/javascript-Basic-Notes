155e1d42080977b2859c90f37452adad18ad0ea5
soni suchit <sonisuchit144@gmail.com>
2023-02-22 19:34:50 +0530

    JavaScript Basics

---
+++ function&operaters.js
@@ -0,0 +1,32 @@
+// Get Message
+function getMessage() {
+    return "hello"
+     
+}
+
+const string = getMessage();
+
+// Add Two Numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+addNumbers(1, 2); //  3
+
+
+// Multiplying Numbers
+
+function product(a,b) {
+    return a*b;
+}
+product(2,4) //8
+
+
+
+// Averaging Numbers
+function average(a, b, c, d) {
+    return average = (a+ b + c+d) / 4;
+
+}
+
+
---
+++ javascriptbasic/datatype.js
@@ -0,0 +1,16 @@
+// string
+const message = "Hello, hope you're having a nice day!"; // Hello, hope you're having a nice day!
+
+
+// Numbers
+const a=4 //4
+const b=7 //7
+
+// Booleans
+const  c=false; //false
+const d=true; //true
+
+// Mutable Let
+// Constants are immutable so Using the keyword let instead of const will allow us to make the value mutable.
+
+let h = 5;
