c5896efad5c48394060e66d227d2dc05c6cb9402
soni suchit <sonisuchit144@gmail.com>
2023-03-16 18:50:48 +0530

    add comment

---
+++ JavaScript Basic Notes/Function&Opeaters.js
@@ -0,0 +1,49 @@
+// Get Message
+function getMessage() {
+    return "hello"
+     
+}
+
+const string = getMessage();
+
+// Add Two Numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+addNumbers(1, 2); //  3
+
+
+// Multiplying Numbers
+
+function product(a,b) {
+    return a*b;
+}
+product(2,4) //8
+
+
+
+// Averaging Numbers
+function average(a, b, c, d) {
+    return average = (a+ b + c+d) / 4;
+
+}
+average (1,1,1,1)// 1
+
+//Number Randomness
+function getRandom() {
+    return Math.random();
+    }
+
+    //EX
+    // randomNumber will be between 25 and 100
+const randomNumber = (Math.random() * 75) + 25; //25-100
+
+   //Floor :- The function will round a number down to the nearest integer. For example if we had the number, 2.9999, the function will round this input down to 2.
+   function getFloor(x) {
+    return Math.floor(2.13434);//2
+    
+}
+
+
+
---
+++ JavaScript Basic Notes/conditionals.js
@@ -0,0 +1,20 @@
+// Let's imagine we're building a website that requires users to have a subscription to access content. We want to encourage users to try it out so we add a free trial.
+
+// Let's update the canAccess function to allow access if the user has either a subscription or a free trial.
+// If either purchasedSubscription or freeTrial are true, return true. Otherwise return false.
+
+function canAccess(purchasedSubscription, freeTrial,Otherwise) {
+    if (purchasedSubscription){
+        return true;
+    }
+    else if (freeTrial){
+        return true;
+    }
+    else (Otherwise)
+    return false;
+
+
+
+}
+
+console.log(canAccess(freeTrial));
